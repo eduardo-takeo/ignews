2d072aa4c1745cee1cf38443e8b4b389ba3e6d2b
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-05-23 20:51:29 -0300

    Consume posts from PrismicCMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,23 @@
 import Head from "next/head";
 import React from "react";
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-reactjs";
+import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+interface Post {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: Date;
+}
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -12,18 +26,49 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>20:00</time>
-            <strong>React title</strong>
-            <p>dasdjsakjdskaljdklasjdkljsdklasjlk</p>
-          </a>
-          <a href="#">
-            <time>20:00</time>
-            <strong>React title</strong>
-            <p>dasdjsakjdskaljdklasjdkljsdklasjlk</p>
-          </a>
+          {posts?.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at("document.type", "post")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
